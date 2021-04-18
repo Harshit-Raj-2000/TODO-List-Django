f36ef50b56de33ed6f9185fef3c7af8d7afd9268
Harshit-Raj-2000 <52253887+Harshit-Raj-2000@users.noreply.github.com>
2021-04-18 23:53:22 +0530

    finished styling the page

--- Todo/views.py
+++ Todo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+
+class new_task_form(forms.Form):
+    task = forms.CharField(label="Add Task")
 
 # Create your views here.
 
+tasks = ['foo', 'bar', 'baz', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur eligendi reprehenderit cum corrupti. Eligendi placeat voluptate illo alias fugiat ullam, dolorum cupiditate voluptatum sit consequatur, iure a facilis molestiae ab!']
+
 def index(request):
-    return HttpResponse("TODO List")
+    return render(request, "Todo/tasks.html",{
+        "tasks": tasks,
+        "form": new_task_form()
+    })
+
